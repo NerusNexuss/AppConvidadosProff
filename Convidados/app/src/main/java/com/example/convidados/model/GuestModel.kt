bb8b6084fa32890eacc84c68d15bf29cbf723eb9
NerusNexuss <108414450+NerusNexuss@users.noreply.github.com>
2024-12-05 21:48:40 -0300

    .
    .

---
+++ Convidados/app/src/main/java/com/example/convidados/model/GuestModel.kt
@@ -0,0 +1,6 @@
+package com.example.convidados.model
+
+data class GuestModel(val id: Int, var name: String, var presence: Boolean) {
+
+}
+
---
+++ Convidados/app/src/main/java/com/example/convidados/view/viewholder/GuestViewHolder.kt
@@ -0,0 +1,38 @@
+package com.example.convidados.view.viewholder
+
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.recyclerview.widget.RecyclerView
+import com.example.convidados.databinding.RowGuestBinding
+import com.example.convidados.model.GuestModel
+import com.example.convidados.view.listener.OnGuestListener
+
+class GuestViewHolder(private val bind: RowGuestBinding, private val listener: OnGuestListener) :
+    RecyclerView.ViewHolder(bind.root) {
+
+    fun bind(guest: GuestModel) {
+        //Utilizando viewBinding
+        bind.textName.text = guest.name
+
+        bind.textName.setOnClickListener {
+            listener.onClick(guest.id)
+        }
+
+        bind.textName.setOnLongClickListener {
+            AlertDialog.Builder(itemView.context)
+                .setTitle("Remoção de convidado")
+                .setMessage("Deseja remover?")
+                .setPositiveButton("Sim"
+                ) { dialog, which ->
+                    listener.onDelete(guest.id)
+                }
+                .setNegativeButton("Não", null)
+                .create()
+                .show()
+
+            true
+        }
+
+    }
+
+}
